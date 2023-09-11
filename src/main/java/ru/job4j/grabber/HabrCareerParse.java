34b5101e4314e65bc2dfcae8cb5623554e5df974
Artyrio226 <artur_sar_80@mail.ru>
2023-09-11 16:45:52 +0300

    2.2. Модель данных - Post. [#285211]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -16,6 +16,27 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
+    private String retrieveDescription(String link) {
+        final String[] result = new String[1];
+        Connection connection = Jsoup.connect(link);
+        Document document = null;
+        try {
+            document = connection.get();
+        } catch (IOException e) {
+            System.out.printf("Invalid link %s%n", link);
+        }
+        assert document != null;
+        Elements rows = document.select(".basic-section--appearance-vacancy-description");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".basic-section--appearance-vacancy-description > h2:nth-child(1)").first();
+            Element discrElement = row.select(".vacancy-description__text").first();
+            String titleName = titleElement.text();
+            String discrName = discrElement.text();
+            result[0] = String.format("%s%n%s%n", titleName, discrName);
+        });
+        return result[0];
+    }
+
     public static void main(String[] args) throws IOException {
         for (int i = 1; i < 6; i++) {
             Connection connection = Jsoup.connect(String.format("%s?page=%s", PAGE_LINK, i));
@@ -28,7 +49,8 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 LocalDate date = LocalDate.parse(dateElement.attr("datetime"), DateTimeFormatter.ISO_DATE_TIME);
-                System.out.printf("%s %s %s%n", vacancyName, link, date);
+                String rd = new HabrCareerParse().retrieveDescription(link);
+                System.out.printf("%s%n%s%n%s%n%n", date, vacancyName, rd);
             });
         }
     }
